fcdab0f38da9c3dbe6241e9fc4a3d11b74c2988c
Quentin Castel <quentin.castel@yapily.com>
2020-11-13 10:25:35 +0000

    Remove my testing

--- release-github-actions.sh
+++ release-github-actions.sh
@@ -3,12 +3,4 @@
 export CI_COMMIT_SHA="${GITHUB_SHA}"
 export CI_COMMIT_REF_NAME="${GITHUB_REF}"
 
-add-ssh-key.sh
-
-git clone --verbose git@github.com:idhub-io/idhub-api.git
-if [[ "$?" -ne 0 ]] ; then
-  echo "Git clone failed"
-  exit 1
-fi
-
 release.sh
